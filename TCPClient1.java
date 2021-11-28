4628c488319bae3f0544e9f8b95e91696fa04632
KarsonHyland <78519480+KarsonHyland@users.noreply.github.com>
2021-11-28 14:27:29 -0600

    Professors test code

--- TCPClient1.java
+++ TCPClient1.java
@@ -1,9 +1,29 @@
-package k;
+import java.io.*;
+import java.net.*;
 
-public class TCPClient1 {
+class TCPClient1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String sentence;
+		String modifiedSentence;
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+		Socket clientSocket = new Socket("127.0.0.1", 6789);
+
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		sentence = inFromUser.readLine();
+
+		outToServer.writeBytes(sentence + '\n');
+
+		modifiedSentence = inFromServer.readLine();
+
+		System.out.println("FROM SERVER: " + modifiedSentence);
+
+		clientSocket.close();
 
 	}
 
