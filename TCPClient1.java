128d7f62186e5c455f30b65599a1e249a5512ac2
Karson H <78519480+KarsonHyland@users.noreply.github.com>
2021-11-28 15:56:22 -0600

    Update TCPClient1.java
    client is done

--- TCPClient1.java
+++ TCPClient1.java
@@ -3,28 +3,46 @@ import java.net.*;
 
 class TCPClient1 {
 
-	public static void main(String[] args) {
-		String sentence;
-		String modifiedSentence;
+    public static void main(String[] args) {
+        String sentence;
+        String modifiedSentence;
+        String userEqaution;
 
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-		Socket clientSocket = new Socket("127.0.0.1", 6789);
+        Socket clientSocket = new Socket("127.0.0.1", 6789);
 
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-		sentence = inFromUser.readLine();
+        sentence = inFromUser.readLine();
 
-		outToServer.writeBytes(sentence + '\n');
+        outToServer.writeBytes(sentence + '\n');
 
-		modifiedSentence = inFromServer.readLine();
+        modifiedSentence = inFromServer.readLine();
 
-		System.out.println("FROM SERVER: " + modifiedSentence);
+        System.out.println("FROM SERVER: " + modifiedSentence);
 
-		clientSocket.close();
+        if (modifiedSentence.equals("OK")) {
+            System.out.println(
+                    "The client has successfully connected to the server! \n please send equations in the following format:\nnumber - number\nnumber + number\nnumber / number\nnumber * number");
+        } else {
+            System.out.println("connection to the server failed");
+            return;
+        }
 
-	}
+        do {
+            System.out.println("Enter the equation you want to send to the server:");
+            userEqaution = inFromUser.readLine();
+            System.out.println("sending your equation to the server");
+            outToServer.writeBytes(userEqaution + '\n');
+            modifiedSentence = inFromServer.readLine();
+            System.out.println("The server said that " + userEqaution + " = " + modifiedSentence);
+        } while (!modifiedSentence.equals("exit"));
+
+        clientSocket.close();
+
+    }
 
 }
