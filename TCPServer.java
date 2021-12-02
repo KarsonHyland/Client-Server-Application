478b21bfa3ef292b421865209672f44ce2265e5d
Karson H <78519480+KarsonHyland@users.noreply.github.com>
2021-12-01 19:50:02 -0600

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -3,27 +3,102 @@ import java.net.*;
 
 public class TCPServer {
 
-	public static void main(String[] args) {
-		String clientSentence;
-		String capitalizedSentence;
+    public static void main(String[] args) throws IOException {
 
-		ServerSocket welcomeSocket = new ServerSocket(6789);
+        ServerSocket welcomeSocket = new ServerSocket(6789);
 
-		while (true) {
+        // Socket connectionSocket = welcomeSocket.accept();
+        while (true) {
 
-			Socket connectionSocket = welcomeSocket.accept();
+            Socket connectionSocket = welcomeSocket.accept();
 
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            System.out.println("new client is connected");
 
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            ClientHandler clientSock = new ClientHandler(connectionSocket);
 
-			clientSentence = inFromClient.readLine();
+            new Thread(clientSock).start();
 
-			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+        }
 
-			outToClient.writeBytes(capitalizedSentence);
-		}
+    }
 
-	}
+}
 
+// ClientHandler class
+class ClientHandler implements Runnable {
+    private final Socket connectionSocket;
+
+    // Constructor
+    public ClientHandler(Socket socket) {
+        this.connectionSocket = socket;
+    }
+
+    public void run() {
+        DataOutputStream outToClient = null;
+        BufferedReader in = null;
+        int mathOpperation;
+        int answer = 0;
+        String clientSentence;
+        String answerString;
+        try {
+
+            // get the outputstream of client
+            outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            // get the inputstream of client
+            in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            while ((clientSentence = in.readLine()) != null) {
+
+                System.out.println("The eqaution sent was " + clientSentence);
+
+                if (clientSentence.indexOf("+") != -1) {
+                    mathOpperation = clientSentence.indexOf("+");
+                    answer = Integer.parseInt(clientSentence.substring(0, mathOpperation))
+                            + Integer.parseInt(clientSentence.substring(mathOpperation + 1));
+                }
+
+                else if (clientSentence.indexOf("-") != -1) {
+                    mathOpperation = clientSentence.indexOf("-");
+                    answer = Integer.parseInt(clientSentence.substring(0, mathOpperation))
+                            - Integer.parseInt(clientSentence.substring(mathOpperation + 1));
+                }
+
+                else if (clientSentence.indexOf("/") != -1) {
+                    mathOpperation = clientSentence.indexOf("/");
+                    answer = Integer.parseInt(clientSentence.substring(0, mathOpperation))
+                            / Integer.parseInt(clientSentence.substring(mathOpperation + 1));
+                }
+
+                else if (clientSentence.indexOf("*") != -1) {
+                    mathOpperation = clientSentence.indexOf("*");
+                    answer = Integer.parseInt(clientSentence.substring(0, mathOpperation))
+                            * Integer.parseInt(clientSentence.substring(mathOpperation + 1));
+                }
+
+                if (clientSentence.equals("Close Client")) {
+                    answerString = "close";
+                } else {
+                    System.out.println("The equation sent was " + clientSentence + " and the answer is " + answer);
+                    answerString = String.valueOf(answer);
+                }
+
+                outToClient.writeBytes(answerString + '\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (outToClient != null) {
+                    outToClient.close();
+                }
+                if (in != null) {
+                    in.close();
+                    connectionSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
